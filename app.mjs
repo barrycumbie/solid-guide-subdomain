8ab78ed977abee466fc594385b1477198028dec1
Cumbie <bcumbie@CSM2-0244-MFXXH.main.local.una.edu>
2025-11-12 12:35:26 -0600

    turn on node app

---
+++ app.mjs
@@ -0,0 +1,23 @@
+import 'dotenv/config';
+import express from 'express';
+// import { fileURLToPath } from 'url';
+// import { dirname, join } from 'path';
+
+//setup express app
+const app = express();
+const PORT = process.env.PORT || 3000;
+const IP_ADD = process.env.IP_ADD;
+app.use(express.json());
+
+
+// const __filename = fileURLToPath(import.meta.url);
+// const __dirname = dirname(__filename);
+// app.use(express.static(join(__dirname, 'public')));
+
+app.get('/', (req, res) => {
+  res.send('Hello Express from CI/D 😍😍😍.')
+})
+
+app.listen(PORT, () => {
+  console.log(`Example app listening on port ${PORT}`)
+})
